cd0e4cc71079d6be41f3cc1b14004fcdb07e3eda
GiSDeCain <gisdecain8@gmail.com>
2017-10-23 15:21:23 +0200

    class contact created

---
+++ model/contact.py
@@ -0,0 +1,18 @@
+class Contact:
+    def __init__(self, firstName, middleName, lastName, nickName,title, company, address, homeNumber, mobileNumber, workNumber, faxNumber, address2, phoneNumber2, notes):
+        self.firstName = firstName
+        self.middlename = middleName
+        self.lastName = lastName
+        self.nickName = nickName
+        self.title = title
+        self.company = company
+        self.address = address
+        self.homeNumber = homeNumber
+        self.mobileNumber = mobileNumber
+        self.workNumber = workNumber
+        self.faxNumber = faxNumber
+        self.address2 = address2
+        self.phoneNumber2 = phoneNumber2
+        self.notes = notes
+
+__author__ = 'GiSDeCain'
