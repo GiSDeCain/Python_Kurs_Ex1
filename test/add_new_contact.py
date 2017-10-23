00c30800419b2be027181ec988d1c6cf14768a88
GiSDeCain <gisdecain8@gmail.com>
2017-10-23 16:59:10 +0200

    minor changes

--- test/add_new_contact.py
+++ test/add_new_contact.py
@@ -3,7 +3,7 @@ from model.contact import Contact
 
 
 def add_new_contact(app):
-    app.session.login()
+    app.session.login(username="admin", password="secret")
     app.contact.add_new_contact(Contact(firstName="gfhfghh", middleName="xgnhghhc", lastName="xgnhcgh", nickName="cxgbncgb",
                                         title="xgbxgb", company="xgnxcfgnn", address="cghnhmnmvghn",
                                         homeNumber="465567546", mobileNumber="64745467", workNumber="64745746", faxNumber="467467456",
